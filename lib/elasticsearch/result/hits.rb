15c0fac2d5532557ec3dc48872d31ee4515d9d6c
Markus Schirp <mbj@seonic.net>
2012-11-30 15:37:26 +0100

    Add Result::Hits#size

--- lib/elasticsearch/result/hits.rb
+++ lib/elasticsearch/result/hits.rb
@@ -37,13 +37,23 @@ module Elasticsearch
       # @api private
       #
       def contents
-        data.fetch('hits').map do |hit|
+        raw_hits.map do |hit|
           Hit.new(hit)
         end
       end
       memoize :contents
+
+      # Return amount of hits in this request
+      #
+      # @return [Fixnum]
+      #
+      # @api privte
+      #
+      def size
+        raw_hits.size
+      end
       
-      # Return total amount of hits
+      # Return total amount of hits in the query
       #
       # @return [Fixnum]
       #
@@ -65,6 +75,16 @@ module Elasticsearch
       #
       def initialize(data)
         @data = data
+      end
+
+      # Return raw hits
+      #
+      # @return [Array]
+      #
+      # @api private
+      #
+      def raw_hits
+        data.fetch('hits')
       end
 
     end
