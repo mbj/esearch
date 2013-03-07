94d6fa3880b86ce792ea6a41ad18c016f67923e7
Markus Schirp <mbj@schirp-dso.com>
2013-03-07 22:10:27 +0100

    Define meta methods with explicit empty block

--- lib/elasticsearch/presenter.rb
+++ lib/elasticsearch/presenter.rb
@@ -14,7 +14,7 @@ module Elasticsearch
     #
     def self.expose_tagged_collection(key, presenter)
       key = key.to_s # caches string in closure ;)
-      define_method(key) do
+      define_method(key) do ||
         raw.fetch(key).map do |name, element| 
           presenter.new(element.merge('name' => name))
         end
@@ -34,7 +34,7 @@ module Elasticsearch
     #
     def self.expose_primitive(key, method_name = key)
       key = key.to_s # caches string in closure ;)
-      define_method(method_name) do
+      define_method(method_name) do ||
         raw.fetch(key)
       end
     end
