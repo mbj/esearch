27dcc3cd1a631f4ec1bdf7ded5a08250fd3460b0
Markus Schirp <mbj@schirp-dso.com>
2013-04-24 19:09:07 +0200

    Expose primitives in cluster health presenter

--- lib/elasticsearch/presenter/cluster.rb
+++ lib/elasticsearch/presenter/cluster.rb
@@ -6,6 +6,78 @@ module Elasticsearch
       # Presenter for cluster health
       class Health < self
 
+        # Return cluster name
+        #
+        # @return [String]
+        #
+        # @api private
+        #
+        expose_primitive('name')
+
+        # Return status string
+        #
+        # @return [String]
+        #
+        # @api private
+        #
+        expose_primitive('status')
+
+        # Return number of nodes
+        #
+        # @return [String]
+        #
+        # @api private
+        #
+        expose_primitive('number_of_nodes')
+
+        # Return number of data nodes
+        #
+        # @return [Fixnum]
+        #
+        # @api private
+        #
+        expose_primitive('number_of_data_nodes')
+
+        # Return number of active primary shards
+        #
+        # @return [Fixnum]
+        #
+        # @api private
+        #
+        expose_primitive('active_primary_shards')
+
+        # Return number of active shards
+        #
+        # @return [Fixnum]
+        #
+        # @api private
+        #
+        expose_primitive('active_shards')
+
+        # Return number of relocating shards
+        #
+        # @return [Fixnum]
+        #
+        # @api private
+        #
+        expose_primitive('relocating_shards')
+
+        # Return number of initializing shards
+        #
+        # @return [Fixnum]
+        #
+        # @api private
+        #
+        expose_primitive('initializing_shards')
+
+        # Return number of unassigned shards
+        #
+        # @return [Fixnum]
+        #
+        # @api private
+        #
+        expose_primitive('unassigned_shards')
+
       end
     end
   end
